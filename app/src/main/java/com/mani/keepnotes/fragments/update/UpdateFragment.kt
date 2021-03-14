44d6453e9b826ed2222cefe01cef6ecadb878a57
Manikant Rai <manikant.rai2000@gmail.com>
2021-03-15 01:37:30 +0530

    menu created for update fragment

--- app/src/main/java/com/mani/keepnotes/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/mani/keepnotes/fragments/update/UpdateFragment.kt
@@ -1,10 +1,8 @@
 package com.mani.keepnotes.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.mani.keepnotes.R
 
 class UpdateFragment : Fragment() {
@@ -14,7 +12,13 @@ class UpdateFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        // Set Menu
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
     }
 
 }
