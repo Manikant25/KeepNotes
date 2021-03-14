117bc6a84d0fdfc0e655b96c46732e7baccdff33
Manikant Rai <manikant.rai2000@gmail.com>
2021-03-15 00:09:34 +0530

    supportNaviagte up added

--- app/src/main/java/com/mani/keepnotes/MainActivity.kt
+++ app/src/main/java/com/mani/keepnotes/MainActivity.kt
@@ -2,12 +2,21 @@ package com.mani.keepnotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.mani.keepnotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.navhost_fragment))
+    }
+//To change action bar with fragment
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navhost_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/mani/keepnotes/fragments/list/ListFragment.kt
+++ app/src/main/java/com/mani/keepnotes/fragments/list/ListFragment.kt
@@ -5,16 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.mani.keepnotes.R
+import com.mani.keepnotes.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
-
+lateinit var binding : FragmentListBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+//        val view =inflater.inflate(R.layout.fragment_list, container, false)
+         binding = FragmentListBinding.inflate(inflater,container,false)
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+
+        return binding.root
+
     }
 
 }
