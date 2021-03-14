8ac8f20dc26307d3d028e654f786bb69a11f898e
Manikant Rai <manikant.rai2000@gmail.com>
2021-03-15 00:57:01 +0530

    menu for add fragment added

--- app/src/main/java/com/mani/keepnotes/fragments/add/AddFragment.kt
+++ app/src/main/java/com/mani/keepnotes/fragments/add/AddFragment.kt
@@ -1,10 +1,8 @@
 package com.mani.keepnotes.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.mani.keepnotes.R
 
 class AddFragment : Fragment() {
@@ -14,7 +12,13 @@ class AddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        // Set Menu
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 
 }
