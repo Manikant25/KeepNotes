c8f0f8311709c7a8aad6284498544a0451185c79
Manikant Rai <manikant.rai2000@gmail.com>
2021-03-15 03:22:55 +0530

    DAO created

---
+++ app/src/main/java/com/mani/keepnotes/data/models/ToDoDao.kt
@@ -0,0 +1,17 @@
+package com.mani.keepnotes.data.models
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface ToDoDao {
+
+    @Query("SELECT * FROM todo_table ORDER BY id ASC")
+    fun getAllData(): LiveData<List<ToDoData>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertData(toDoData: ToDoData)
+
+    
+
+}
