e345c59d8f69f3949cf77dbb4eeef1d5eff92731
Manikant Rai <manikant.rai2000@gmail.com>
2021-03-14 23:36:45 +0530

    main activity lauched

--- app/src/main/java/com/mani/keepnotes/MainActivity.kt
+++ app/src/main/java/com/mani/keepnotes/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import com.mani.keepnotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
