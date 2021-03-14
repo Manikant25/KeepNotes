6e236a8ff5f92ed2b6dae3a936e89372d2a4c157
Manikant Rai <manikant.rai2000@gmail.com>
2021-03-15 02:44:01 +0530

    entity class created

---
+++ app/src/main/java/com/mani/keepnotes/data/models/ToDoData.kt
@@ -0,0 +1,14 @@
+package com.mani.keepnotes.data.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "todo_table")
+data class ToDoData(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var title: String,
+    var priority: Priority,
+    var description: String
+)
