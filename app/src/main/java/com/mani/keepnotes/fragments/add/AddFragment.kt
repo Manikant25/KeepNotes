1a49fa00779c99a78c980263662e442b0a7d24fc
Manikant Rai <manikant.rai2000@gmail.com>
2021-03-15 18:02:50 +0530

    viewBinding Added

--- app/src/main/java/com/mani/keepnotes/fragments/add/AddFragment.kt
+++ app/src/main/java/com/mani/keepnotes/fragments/add/AddFragment.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import com.mani.keepnotes.R
+import com.mani.keepnotes.databinding.FragmentAddBinding
+import com.mani.keepnotes.databinding.FragmentListBinding
 
 class AddFragment : Fragment() {
-
+    lateinit var binding: FragmentAddBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         // Set Menu
+
+        binding = FragmentAddBinding.inflate(inflater, container, false)
         setHasOptionsMenu(true)
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
